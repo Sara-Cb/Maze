ed00602e5565474d4fdc413e50ff387a1e3ad9d2
Sara-Cb <123389945+Sara-Cb@users.noreply.github.com>
2023-07-20 19:05:38 +0200

    implemented logout, separated creative reducers

---
+++ maze/src/components/elements/LogoutButton.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import { store } from "../../redux/store/store";
+import logoutFetch from "../../redux/actions/logoutAction";
+
+const LogoutButton = () => {
+  const [showModal, setShowModal] = useState(false);
+  const dispatch = store.dispatch;
+
+  const handleLogout = () => {
+    dispatch(logoutFetch());
+    window.location.href = "/";
+    console.log("Logout completed!");
+    setShowModal(false); // Chiude il modale dopo aver effettuato il logout
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false); // Chiude il modale se l'utente clicca "No" o fa clic fuori dal modale
+  };
+
+  const handleShowModal = () => {
+    setShowModal(true); // Apre il modale quando l'utente clicca sul bottone "Logout"
+  };
+
+  return (
+    <>
+      <p className="btnGreenDark" onClick={handleShowModal}>
+        Logout
+      </p>
+      ;
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Logout</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure to logout?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            No
+          </Button>
+          <Button variant="primary" onClick={handleLogout}>
+            Yes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default LogoutButton;
